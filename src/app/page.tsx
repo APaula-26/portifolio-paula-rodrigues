6ddef83878bcaef1bd9c1053948cc070166e97fa
Paula Rodrigues <anapauladesouzarodrigues@protonmail.com>
2025-11-24 19:57:32 -0300

    Ajuste final_comentarios

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,15 @@
 'use client';
+// Avisa ao Next.js que este arquivo deve ser renderizado no Navegador (Cliente) e não no Servidor.
+// Necessário pois O Chakra UI usa interatividade (como passar o mouse em botões, animações, onClick), 
+// e essas coisas só funcionam no navegador. Sem isso, o código daria erro.
 
 import { Flex, Heading, Text, Image, Button, HStack, Box, Container, SimpleGrid, Icon, Link, VStack } from "@chakra-ui/react";
 import { FaPython, FaPhp, FaJava, FaJs, FaLaravel, FaReact, FaNodeJs, FaGithub, FaExternalLinkAlt, FaLinkedin, FaEnvelope, FaPhone } from "react-icons/fa";
 import { SiChakraui } from "react-icons/si";
+
+// @chakra-ui/react: Importa os "blocos de construção" visuais. Em vez de escrever 
+//<div> ou <h1> e estilizar com CSS, usamos componentes prontos como <Box> (caixa), <Text> (texto), <Button> (botão). 
+//react-icons/fa: Importa os ícones (símbolos) da biblioteca FontAwesome (ex: o pinguim do Linux, o logo do Python, etc.).
 
 export default function Home() {
 
@@ -12,6 +19,10 @@ export default function Home() {
       element.scrollIntoView({ behavior: "smooth", block: "center" });
     }
   };
+// Para criar navegação ("smooth scroll").
+// Recebe o id da seção (ex: "projetos").
+// Busca esse elemento na tela (document.getElementById).
+// Se achar, rola a tela suavemente até ele (scrollIntoView).
 
   const linguagens = [
     { name: "Python", icon: FaPython, color: "#3776AB" },
@@ -33,6 +44,10 @@ export default function Home() {
     { name: "DSII - Paula Rodrigues", url: "https://github.com/APaula-26/DSII-Paula-Rodrigues" },
   ];
 
+  // Em vez de repetir o código HTML várias vezes para cada ícone ou projeto, 
+  // guardamos as informações em listas (arrays). Depois, usamos o comando 
+  // .map() para gerar o visual automaticamente
+
   // Estilo dos Cartões
   const glassCardStyle = {
     bg: "white",
@@ -43,6 +58,7 @@ export default function Home() {
     transition: "all 0.3s ease",
     _hover: { transform: "translateY(-5px)", boxShadow: "2xl" }
   };
+  // glasscardstyle : objeto JavaScript que guarda regras de estilo CSS.Cria o visual dos cartões brancos
 
   return (
     <Box
@@ -51,14 +67,14 @@ export default function Home() {
       color="black" 
       fontFamily="sans-serif"
       
-      // --- SOLUÇÃO DO ERRO: Usando 'style' do React ---
+  
       // Isso ignora a checagem estrita de tipos do Chakra para backgrounds complexos
       style={{
-        backgroundImage: "url('/fundo_.jpg')",
-        backgroundPosition: "top left",
-        backgroundRepeat: "repeat",
-        backgroundSize: "120px",
-        backgroundAttachment: "fixed"
+        backgroundImage: "url('/fundo_.jpg')", //Define a imagem do fundo 
+        backgroundPosition: "top left", // define o posicionamento da imagem
+        backgroundRepeat: "repeat", // Faz a imagem repetir lado a lado
+        backgroundSize: "120px", // Define o tamanho das imagens 
+        backgroundAttachment: "fixed" //O "truque" do Parallax. Quando você rola o mouse para baixo, o texto se move, mas o fundo fica parado.
       }}
       // -----------------------------------------------
     >
@@ -66,11 +82,11 @@ export default function Home() {
       {/* --- MENU FLUTUANTE --- */}
       <Flex
         as="nav"
-        position="fixed"
+        position="fixed" //Fixa o menu, fica congelado
         top="20px"
         left="50%"
         transform="translateX(-50%)"
-        zIndex="999"
+        zIndex="999" //Garante que o menu fique por cima de todo o resto do site (camada superior).
         bg="rgba(255,255,255,0.95)"
         backdropFilter="blur(15px)"
         borderRadius="full"
@@ -81,6 +97,7 @@ export default function Home() {
         justify="center"
       >
         <HStack gap={{ base: 1, md: 6 }}>
+        {/* "Horizontal Stack". Organiza os botões um ao lado do outro automaticamente. */}
           {["sobre", "linguagens", "frameworks", "projetos", "contatos"].map((section) => (
              <Button
                key={section}
@@ -104,7 +121,9 @@ export default function Home() {
         {/* --- SEÇÃO 1: QUEM SOU --- */}
         <Flex
           id="sobre"
-          direction={{ base: "column-reverse", md: "row" }}
+          direction={{ base: "column-reverse", md: "row" }} // Responsividade.
+          // md: O texto fica na esquerda e a imagem na direita (linha).
+          // base: A imagem fica em cima e o texto embaixo (coluna invertida).
           align="center"
           justify="space-between"
           minH="70vh"
@@ -115,6 +134,7 @@ export default function Home() {
             <Box p={6} borderRadius="xl" bg="white" boxShadow="lg">
                 <Heading as="h1" size="3xl" fontWeight="extrabold">
                 Olá, sou a <Text as="span" color="#f07109ff">Paula</Text>! 
+                {/* O comando as serve para mudar a "etiqueta" HTML do componente, mantendo o visual do Chakra. */}
                 </Heading>
 
                 <VStack gap={4} align="stretch" mt={4}>
@@ -135,7 +155,7 @@ export default function Home() {
                 bg="#2B6CB0" 
                 color="white" 
                 w="fit-content"
-                _hover={{ bg: "white", color: "#2B6CB0", border: "1px solid #2B6CB0" }}
+                _hover={{ bg: "white", color: "#2B6CB0", border: "1px solid #2B6CB0" }} //_hover) para interatividade
                 onClick={() => scrollToSection('projetos')}
             >
                 Ver meus projetos
@@ -215,11 +235,12 @@ export default function Home() {
                     <Link 
                     key={index} 
                     href={proj.url} 
-                    target="_blank" 
-                    rel="noopener noreferrer"
+                    target="_blank" //Obriga o navegador a abrir o link em uma nova aba, para o usuário não sair do portfólio.
+                    rel="noopener noreferrer" // É uma medida de segurança padrão na web para evitar que o site novo tenha acesso indevido à página original.
                     _hover={{ textDecoration: 'none' }}
                     w="100%" 
                     maxW="600px" 
+                    color="black"
                     >
                     <HStack 
                         bg="gray.100" 
@@ -265,13 +286,13 @@ export default function Home() {
                             borderRadius="full" 
                             bg="gray.100"
                             align="center" justify="center"
-                            _groupHover={{ bg: "#2B6CB0", color: "white", transform: "scale(1.1)" }}
+                            _groupHover={{ bg: "#2B6CB0", color: "black", transform: "scale(1.1)" }}
                             transition="all 0.3s"
                         >
-                            <Icon as={FaEnvelope} w={6} h={6} color="black" _groupHover={{ color: "white" }} />
+                            <Icon as={FaEnvelope} w={6} h={6} color="black" _groupHover={{ color: "black" }} />
                         </Flex>
-                        <Text fontWeight="bold">Email</Text>
-                        <Text fontSize="sm" opacity={0.8}>ana.rodrigues490@etec.sp.gov.br</Text>
+                        <Text fontWeight="bold" color="black">Email</Text>
+                        <Text fontSize="sm" opacity={0.8} color="black">ana.rodrigues490@etec.sp.gov.br</Text>
                     </VStack>
                 </Link>
 
@@ -285,7 +306,7 @@ export default function Home() {
                         _hover={{ bg: "#2B6CB0", color: "white", transform: "scale(1.1)" }}
                         transition="all 0.3s"
                     >
-                        <Icon as={FaPhone} w={6} h={6} color="black" _hover={{ color: "white" }} />
+                        <Icon as={FaPhone} w={6} h={6} color="black" _hover={{ color: "black" }} />
                     </Flex>
                     <Text fontWeight="bold">Telefone</Text>
                     <Text fontSize="sm" opacity={0.8}>(11) 99999-9999</Text>
@@ -309,8 +330,8 @@ export default function Home() {
                         >
                             <Icon as={FaLinkedin} w={6} h={6} color="black" _groupHover={{ color: "white" }} />
                         </Flex>
-                        <Text fontWeight="bold">Linkedin</Text>
-                        <Text fontSize="sm" opacity={0.8}>Ana Paula</Text>
+                        <Text fontWeight="bold" color="black">Linkedin</Text>
+                        <Text fontSize="sm" opacity={0.8} color="black">Ana Paula</Text>
                     </VStack>
                 </Link>
 
